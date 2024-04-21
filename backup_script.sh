ad31489966d0feacf59d0b6a65905911a69fc5cd
Anna_Lozovytska <lozovytska.anna@gmail.com>
2024-04-21 16:38:15 +0300

    add backup_script file

---
+++ backup_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Шлях до директорії для бекапу
+BACKUP_SRC="/home/username/data"
+# Шлях, куди буде збережено бекап
+BACKUP_DEST="/home/username/backups"
+# Ім'я файлу бекапу
+BACKUP_NAME="backup_$(date +%Y%m%d_%H%M%S).tar.gz"
+
+# Створення директорії для бекапів, якщо вона не існує
+mkdir -p $BACKUP_DEST
+
+# Створення архіву
+tar -czf $BACKUP_DEST/$BACKUP_NAME $BACKUP_SRC
+
+# Повідомлення про завершення бекапу
+echo "Backup created at $BACKUP_DEST/$BACKUP_NAME"
